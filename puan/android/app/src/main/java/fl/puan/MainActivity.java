97babfd0e0e99740aae335a48f403d8b7a358066
ssj4dofuturo <felip3.fl@gmail.com>
2021-03-30 21:18:13 -0300

    First Commit

---
+++ puan/android/app/src/main/java/fl/puan/MainActivity.java
@@ -0,0 +1,6 @@
+package fl.puan;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ puan/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:puan/ui/home_page.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
