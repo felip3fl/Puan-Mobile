66f46d3fd75ef63c0b8272db86b9c5f2b15df4da
ssj4dofuturo <felip3.fl@gmail.com>
2021-04-12 08:02:50 -0300

    Separacao metodos; Percentual relogio finalizado

--- puan/lib/ui/screens/home_screen.dart
+++ puan/lib/ui/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:decorated_icon/decorated_icon.dart';
 import 'package:flutter/services.dart';
+import 'package:puan/ui/widgets/FaceRelogio.dart';
 
 class HomeScreen extends StatelessWidget {
   var cor1 = Color.fromARGB(255, 239, 242, 247);
@@ -29,12 +30,10 @@ class HomeScreen extends StatelessWidget {
             padding: EdgeInsets.fromLTRB(30, 55, 30, 0),
             child: Column(
               children: <Widget>[
-                // Container(
-                //   child: CustomPaint(painter: FaceCisculo()),
-                // ),
-                // Container(
-                //   child: CustomPaint(painter: FaceCisculo2()),
-                // ),
+                // Stack(children: <Widget>[
+                //   CustomPaint(painter: FaceCisculo()),
+                //   CustomPaint(painter: FaceCisculo2()),
+                // ]),
                 Container(
                   child: Row(
                     children: <Widget>[
@@ -95,7 +94,7 @@ class HomeScreen extends StatelessWidget {
                         child: Icon(
                           Icons.more_vert,
                           color: fonteTitulo,
-                          size: 30.0,
+                          size: 28.0,
                           semanticLabel: 'Menu',
                         ),
                       )
@@ -138,16 +137,19 @@ class HomeScreen extends StatelessWidget {
                               ],
                             ),
                           ),
-                          Text(
-                            "Iniciar",
-                            style: TextStyle(
-                                decoration: TextDecoration.none,
-                                fontFamily: 'WorkSans-Thin',
-                                fontSize: 70,
-                                color: fonteBotaoRelogio),
+                          GestureDetector(
+                            child: Text(
+                              "Iniciar",
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontFamily: 'WorkSans-Thin',
+                                  fontSize: 70,
+                                  color: fonteBotaoRelogio),
+                            ),
+                            onTap: (){},
                           ),
                           Container(
-                            child: CustomPaint(painter: FaceRelogio()),
+                            child: CustomPaint(painter: FaceRelogio(15)),
                           ),
                         ],
                       )),
@@ -223,281 +225,6 @@ class FaceCisculo2 extends CustomPainter {
 
     // Right eye
     canvas.drawCircle(Offset(500, 510), 390, paint);
-  }
-
-  @override
-  bool shouldRepaint(FaceRelogio oldDelegate) => false;
-}
-
-class FaceRelogio extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    // Define a paint object
-    final paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 3.0
-      ..strokeCap = StrokeCap.round
-      ..color = Color.fromARGB(255, 200, 200, 200);
-
-    final paint2 = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 3.0
-      ..strokeCap = StrokeCap.round
-      ..color = Color.fromARGB(255, 0, 0, 0);
-
-    var Cosseno = new List<double>();
-    Cosseno.add(1);
-    Cosseno.add(0.9998);
-    Cosseno.add(0.9994);
-    Cosseno.add(0.9986);
-    Cosseno.add(0.9976);
-    Cosseno.add(0.9962);
-    Cosseno.add(0.9945);
-    Cosseno.add(0.9925);
-    Cosseno.add(0.9903);
-    Cosseno.add(0.9877);
-    Cosseno.add(0.9848);
-    Cosseno.add(0.9816);
-    Cosseno.add(0.9781);
-    Cosseno.add(0.9744);
-    Cosseno.add(0.9703);
-    Cosseno.add(0.9659);
-    Cosseno.add(0.9613);
-    Cosseno.add(0.9563);
-    Cosseno.add(0.9511);
-    Cosseno.add(0.9455);
-    Cosseno.add(0.9397);
-    Cosseno.add(0.9336);
-    Cosseno.add(0.9272);
-    Cosseno.add(0.9205);
-    Cosseno.add(0.9135);
-    Cosseno.add(0.9063);
-    Cosseno.add(0.8988);
-    Cosseno.add(0.891);
-    Cosseno.add(0.8829);
-    Cosseno.add(0.8746);
-    Cosseno.add(0.866);
-    Cosseno.add(0.8572);
-    Cosseno.add(0.848);
-    Cosseno.add(0.8387);
-    Cosseno.add(0.829);
-    Cosseno.add(0.8192);
-    Cosseno.add(0.809);
-    Cosseno.add(0.7986);
-    Cosseno.add(0.788);
-    Cosseno.add(0.7771);
-    Cosseno.add(0.766);
-    Cosseno.add(0.7547);
-    Cosseno.add(0.7431);
-    Cosseno.add(0.7314);
-    Cosseno.add(0.7193);
-    Cosseno.add(0.7071);
-    Cosseno.add(0.6947);
-    Cosseno.add(0.682);
-    Cosseno.add(0.6691);
-    Cosseno.add(0.6561);
-    Cosseno.add(0.6428);
-    Cosseno.add(0.6293);
-    Cosseno.add(0.6157);
-    Cosseno.add(0.6018);
-    Cosseno.add(0.5878);
-    Cosseno.add(0.5736);
-    Cosseno.add(0.5592);
-    Cosseno.add(0.5446);
-    Cosseno.add(0.5299);
-    Cosseno.add(0.515);
-    Cosseno.add(0.5);
-    Cosseno.add(0.4848);
-    Cosseno.add(0.4695);
-    Cosseno.add(0.454);
-    Cosseno.add(0.4384);
-    Cosseno.add(0.4226);
-    Cosseno.add(0.4067);
-    Cosseno.add(0.3907);
-    Cosseno.add(0.3746);
-    Cosseno.add(0.3584);
-    Cosseno.add(0.342);
-    Cosseno.add(0.3256);
-    Cosseno.add(0.309);
-    Cosseno.add(0.2924);
-    Cosseno.add(0.2756);
-    Cosseno.add(0.2588);
-    Cosseno.add(0.2419);
-    Cosseno.add(0.225);
-    Cosseno.add(0.2079);
-    Cosseno.add(0.1908);
-    Cosseno.add(0.1736);
-    Cosseno.add(0.1564);
-    Cosseno.add(0.1392);
-    Cosseno.add(0.1219);
-    Cosseno.add(0.1045);
-    Cosseno.add(0.0872);
-    Cosseno.add(0.0698);
-    Cosseno.add(0.0523);
-    Cosseno.add(0.0349);
-    Cosseno.add(0.0175);
-    Cosseno.add(0);
-
-    var Seno = new List<double>();
-    Seno.add(0);
-    Seno.add(0.0175);
-    Seno.add(0.0349);
-    Seno.add(0.0523);
-    Seno.add(0.0698);
-    Seno.add(0.0872);
-    Seno.add(0.1045);
-    Seno.add(0.1219);
-    Seno.add(0.1392);
-    Seno.add(0.1564);
-    Seno.add(0.1736);
-    Seno.add(0.1908);
-    Seno.add(0.2079);
-    Seno.add(0.225);
-    Seno.add(0.2419);
-    Seno.add(0.2588);
-    Seno.add(0.2756);
-    Seno.add(0.2924);
-    Seno.add(0.309);
-    Seno.add(0.3256);
-    Seno.add(0.342);
-    Seno.add(0.3584);
-    Seno.add(0.3746);
-    Seno.add(0.3907);
-    Seno.add(0.4067);
-    Seno.add(0.4226);
-    Seno.add(0.4384);
-    Seno.add(0.454);
-    Seno.add(0.4695);
-    Seno.add(0.4848);
-    Seno.add(0.5);
-    Seno.add(0.515);
-    Seno.add(0.5299);
-    Seno.add(0.5446);
-    Seno.add(0.5592);
-    Seno.add(0.5736);
-    Seno.add(0.5878);
-    Seno.add(0.6018);
-    Seno.add(0.6157);
-    Seno.add(0.6293);
-    Seno.add(0.6428);
-    Seno.add(0.6561);
-    Seno.add(0.6691);
-    Seno.add(0.682);
-    Seno.add(0.6947);
-    Seno.add(0.7071);
-    Seno.add(0.7193);
-    Seno.add(0.7314);
-    Seno.add(0.7431);
-    Seno.add(0.7547);
-    Seno.add(0.766);
-    Seno.add(0.7771);
-    Seno.add(0.788);
-    Seno.add(0.7986);
-    Seno.add(0.809);
-    Seno.add(0.8192);
-    Seno.add(0.829);
-    Seno.add(0.8387);
-    Seno.add(0.848);
-    Seno.add(0.8572);
-    Seno.add(0.866);
-    Seno.add(0.8746);
-    Seno.add(0.8829);
-    Seno.add(0.891);
-    Seno.add(0.8988);
-    Seno.add(0.9063);
-    Seno.add(0.9135);
-    Seno.add(0.9205);
-    Seno.add(0.9272);
-    Seno.add(0.9336);
-    Seno.add(0.9397);
-    Seno.add(0.9455);
-    Seno.add(0.9511);
-    Seno.add(0.9563);
-    Seno.add(0.9613);
-    Seno.add(0.9659);
-    Seno.add(0.9703);
-    Seno.add(0.9744);
-    Seno.add(0.9781);
-    Seno.add(0.9816);
-    Seno.add(0.9848);
-    Seno.add(0.9877);
-    Seno.add(0.9903);
-    Seno.add(0.9925);
-    Seno.add(0.9945);
-    Seno.add(0.9962);
-    Seno.add(0.9976);
-    Seno.add(0.9986);
-    Seno.add(0.9994);
-    Seno.add(0.9998);
-    Seno.add(1);
-
-    final tamanhoRelogio = 165;
-    final tamanhoSetaRelogio = tamanhoRelogio - 15;
-    final tamanhoSetaRelogio2 = tamanhoRelogio - 20;
-    final tamanhoSetaRelogio3 = tamanhoRelogio + 5;
-    final intervalo = 6;
-
-    for (var i = 0; i < Seno.length; i = i + intervalo) {
-      final p1 = Offset((Seno[i]) * tamanhoSetaRelogio,
-          (Cosseno[i] * -1) * tamanhoSetaRelogio); //Offset(Y, X);
-      final p2 = Offset((Seno[i]) * tamanhoRelogio,
-          (Cosseno[i] * -1) * tamanhoRelogio); //Offset(W, H);
-      canvas.drawLine(p1, p2, paint2);
-    }
-
-    for (var i = 0; i < Seno.length; i = i + intervalo) {
-      final p1 =
-          Offset(Seno[i] * tamanhoSetaRelogio, Cosseno[i] * tamanhoSetaRelogio);
-      final p2 = Offset(Seno[i] * tamanhoRelogio, Cosseno[i] * tamanhoRelogio);
-      canvas.drawLine(p1, p2, paint);
-    }
-
-    for (var i = 0; i < Seno.length; i = i + intervalo) {
-      final p1 = Offset((Seno[i] * -1) * tamanhoSetaRelogio,
-          (Cosseno[i] * -1) * tamanhoSetaRelogio); //Offset(Y, X);
-      final p2 = Offset((Seno[i] * -1) * tamanhoRelogio,
-          (Cosseno[i] * -1) * tamanhoRelogio); //Offset(W, H);
-      canvas.drawLine(p1, p2, paint);
-    }
-
-    for (var i = 0; i < Seno.length; i = i + intervalo) {
-      final p1 = Offset((Seno[i] * -1) * tamanhoSetaRelogio,
-          (Cosseno[i]) * tamanhoSetaRelogio); //Offset(Y, X);
-      final p2 = Offset((Seno[i] * -1) * tamanhoRelogio,
-          (Cosseno[i]) * tamanhoRelogio); //Offset(W, H);
-      canvas.drawLine(p1, p2, paint);
-    }
-
-    if (1 == 1) {
-      final p1 = Offset(
-          Seno[90] * tamanhoSetaRelogio2, Cosseno[90] * tamanhoSetaRelogio2);
-      final p2 = Offset(
-          Seno[90] * tamanhoSetaRelogio3, Cosseno[90] * tamanhoSetaRelogio3);
-      canvas.drawLine(p1, p2, paint);
-    }
-
-    if (1 == 1) {
-      final p1 = Offset(
-          Seno[0] * tamanhoSetaRelogio2, Cosseno[0] * tamanhoSetaRelogio2);
-      final p2 = Offset(
-          Seno[0] * tamanhoSetaRelogio3, Cosseno[0] * tamanhoSetaRelogio3);
-      canvas.drawLine(p1, p2, paint);
-    }
-    if (1 == 1) {
-      final p1 = Offset((Seno[0] * -1) * tamanhoSetaRelogio2,
-          (Cosseno[0] * -1) * tamanhoSetaRelogio2);
-      final p2 = Offset((Seno[0] * -1) * tamanhoSetaRelogio3,
-          (Cosseno[0] * -1) * tamanhoSetaRelogio3);
-      canvas.drawLine(p1, p2, paint2);
-    }
-
-    if (1 == 1) {
-      final p1 = Offset((Seno[90] * -1) * tamanhoSetaRelogio2,
-          (Cosseno[90] * -1) * tamanhoSetaRelogio2);
-      final p2 = Offset((Seno[90] * -1) * tamanhoSetaRelogio3,
-          (Cosseno[90] * -1) * tamanhoSetaRelogio3);
-      canvas.drawLine(p1, p2, paint);
-    }
   }
 
   @override
---
+++ puan/lib/ui/widgets/FaceRelogio.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:puan/datas/Cosseno.dart';
+import 'package:puan/datas/Seno.dart';
+
+class FaceRelogio extends CustomPainter {
+  var _porcentagem = 0;
+  var _porcentagemTemp = 0.0;
+
+  FaceRelogio(int porcentagem) {
+    this._porcentagem = porcentagem;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final estiloNormal = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..strokeCap = StrokeCap.round
+      ..color = Color.fromARGB(255, 200, 200, 200);
+
+    final estiloMarcado = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..strokeCap = StrokeCap.round
+      ..color = Color.fromARGB(255, 0, 0, 0);
+
+    Cosseno cosseno = new Cosseno();
+    Seno seno = new Seno();
+
+    final tamanhoRelogio = 165;
+    final tamanhoSetaRelogio = tamanhoRelogio - 15;
+    final tamanhoSetaRelogio2 = tamanhoRelogio - 20;
+    final tamanhoSetaRelogio3 = tamanhoRelogio + 5;
+    final intervalo = 6;
+
+    //0-25
+    _porcentagemTemp = _porcentagem > 0 ? ((100 / 25) * _porcentagem) : 0; 
+    for (var i = intervalo; i < seno.values.length; i = i + intervalo) {
+      final p1 = Offset((seno.values[i]) * tamanhoSetaRelogio, (cosseno.values[i] * -1) * tamanhoSetaRelogio); //Offset(Y, X);
+      final p2 = Offset((seno.values[i]) * tamanhoRelogio, (cosseno.values[i] * -1) * tamanhoRelogio); //Offset(W, H);
+
+      var valor = (100 / seno.values.length) * i >= _porcentagemTemp;
+      canvas.drawLine(p1, p2, valor ? estiloNormal : estiloMarcado);
+    }
+
+
+
+    //25-50
+    _porcentagemTemp = _porcentagem > 25 ? ((100 / 25) * (_porcentagem-25)) : 0; 
+    for (var i = intervalo; i < seno.values.length; i = i + intervalo) {
+      final p1 = Offset(cosseno.values[i] * tamanhoSetaRelogio, seno.values[i] * tamanhoSetaRelogio);
+      final p2 = Offset(cosseno.values[i] * tamanhoRelogio, seno.values[i] * tamanhoRelogio);
+
+      var valor = (100 / seno.values.length) * i >= _porcentagemTemp;
+      canvas.drawLine(p1, p2, valor ?  estiloNormal : estiloMarcado);
+    }
+
+    //50-75
+    _porcentagemTemp = _porcentagem > 50 ? ((100 / 25) * (_porcentagem-50)) : 0; 
+    for (var i = intervalo; i < seno.values.length; i = i + intervalo) {
+      final p1 = Offset((seno.values[i] * -1) * tamanhoSetaRelogio, (cosseno.values[i]) * tamanhoSetaRelogio); //Offset(Y, X);
+      final p2 = Offset((seno.values[i] * -1) * tamanhoRelogio, (cosseno.values[i]) * tamanhoRelogio); //Offset(W, H);
+
+      var valor = (100 / seno.values.length) * i >= _porcentagemTemp;
+      canvas.drawLine(p1, p2, valor ?  estiloNormal : estiloMarcado);
+    }
+
+    //75-100
+    _porcentagemTemp = _porcentagem > 75 ? ((100 / 25) * (_porcentagem-75)) : 0; 
+    for (var i = intervalo; i < seno.values.length; i = i + intervalo) {
+      final p1 = Offset((cosseno.values[i] * -1) * tamanhoSetaRelogio,(seno.values[i] * -1) * tamanhoSetaRelogio,); //Offset(Y, X);
+      final p2 = Offset((cosseno.values[i] * -1) * tamanhoRelogio, (seno.values[i] * -1) * tamanhoRelogio); //Offset(W, H);
+      canvas.drawLine(p1, p2, estiloNormal);
+
+      var valor = (100 / seno.values.length) * i >= _porcentagemTemp;
+      canvas.drawLine(p1, p2, valor ?  estiloNormal : estiloMarcado);
+    }
+
+
+        canvas.drawLine(
+      Offset((seno.values[0] * -1) * tamanhoSetaRelogio2, (cosseno.values[0] * -1) * tamanhoSetaRelogio2), 
+      Offset((seno.values[0] * -1) * tamanhoSetaRelogio3, (cosseno.values[0] * -1) * tamanhoSetaRelogio3), 
+      _porcentagem > 0 ? estiloMarcado : estiloNormal);
+
+    canvas.drawLine(
+      Offset(seno.values[90] * tamanhoSetaRelogio2,cosseno.values[90] * tamanhoSetaRelogio2), 
+      Offset(seno.values[90] * tamanhoSetaRelogio3,cosseno.values[90] * tamanhoSetaRelogio3), 
+      _porcentagem >= 25 ? estiloMarcado : estiloNormal);
+
+    canvas.drawLine(
+      Offset(seno.values[0] * tamanhoSetaRelogio2, cosseno.values[0] * tamanhoSetaRelogio2), 
+      Offset(seno.values[0] * tamanhoSetaRelogio3, cosseno.values[0] * tamanhoSetaRelogio3), 
+      _porcentagem >= 50 ? estiloMarcado : estiloNormal);
+
+    canvas.drawLine(Offset(
+      (seno.values[90] * -1) * tamanhoSetaRelogio2,(cosseno.values[90] * -1) * tamanhoSetaRelogio2), 
+      Offset((seno.values[90] * -1) * tamanhoSetaRelogio3, (cosseno.values[90] * -1) * tamanhoSetaRelogio3), 
+      _porcentagem >= 75 ? estiloMarcado : estiloNormal);
+
+  }
+
+  @override
+  bool shouldRepaint(FaceRelogio oldDelegate) => false;
+}
